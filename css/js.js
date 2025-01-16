bcd6e7f7d14893a13470d91bdebe5dbea832a6f4
bimela tony <tonybimela@gmail.com>
2025-01-16 11:17:57 +0100

    addingfeatures

--- css/js.js
+++ css/js.js
@@ -1,13 +1,13 @@
-const emojis = ["😊","😊","😂","😂","😎","😎","😁","😁","🤣","🤣","😃","😃","😆","😆","👀","👀"]
-var shuf_emojis = emojis.sort(() => (Math.random() > .5) ? 2 : -1)
-for (var i = 0; i < emojis.length; i++) {
-  let box = document.createElement('div')
+const emojis = ['😊', '😊', '😂','😂', '😎', '😎', '😁', '😁', '🤣', '🤣', '😃', '😃', '😆', '😆', '👀', '👀']
+const shufemojis = emojis.sort(() => (Math.random() > 0.5) ? 2 : -1)
+for (let i = 0; i < emojis.length; i++) {
+  const box = document.createElement('div')
   box.className = 'item'
-  box.innerHTML = shuf_emojis[i]
+  box.innerHTML = shufemojis[i]
 
-  box.onclick = function(){
+  box.onclick =  function (){
     this.classList.add('boxOpen')
-    setTimeout(function(){
+    setTimeout(function (){
       if(document.querySelectorAll('.boxOpen').length > 1){
         if (document.querySelectorAll('.boxOpen')[0].innerHTML == document.querySelectorAll('.boxOpen')[1].innerHTML){
           document.querySelectorAll('.boxOpen')[0].classList.add('boxMatch')
@@ -16,15 +16,15 @@ for (var i = 0; i < emojis.length; i++) {
           document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
           document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
 
-          if(document.querySelectorAll('.boxMatch').length == emojis.length){
+          if (document.querySelectorAll ('.boxMatch').length === emojis.length){
             alert('Congratulaitions🎈🎉 Winner')
           }
         } else {
           document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
           document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
+        }
       }
-      }
-    },500)
+    }, 500)
   }
 
   document.querySelector('.game').appendChild(box)
